21a55a9f565bfe88c244d183a354dcc1ce4ad5e7
Ross Whitehead <rossshaggywhitehead@hotmail.com>
2016-07-14 19:41:13 +0100

    Create solution

---
+++ src/SandboxCore.Data/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SandboxCore.Data.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        [StringLength(200)]
+        public string ProductName { get; set; }
+    }
+}
---
+++ src/SandboxCore.Data/SandboxCoreDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SandboxCore.Data.Models;
+
+namespace SandboxCore.Data
+{
+    public class SandboxCoreDbContext : DbContext
+    {
+        public DbSet<Product> Projects { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SandboxCore;Trusted_Connection=True;");
+        }
+    }
+}
